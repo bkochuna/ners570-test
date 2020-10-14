057b6b7fd6f340650974abbe07831056c801b96d
Brendan Kochunas <bkochuna@umich.edu>
2020-10-14 12:35:13 -0400

    WIP: Adding skeleton constructor/destructor
    Description:
    Adding simple constructor and destructor for SparseMatrix
    that just includes a print statement.

--- src/sparsemat.cpp
+++ src/sparsemat.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 #include "sparsemat.hpp"
+
+using namespace std; 
+using namespace SpMV;
+
+//Defines the Constructor Operation
+template <class fp_type>
+SparseMatrix<fp_type>::SparseMatrix(const size_t nrows, const size_t ncols)
+{
+    cout << "Called SpMV::SparseMatrix<fp_type> constructor!" << endl;
+    _nrows = nrows;
+    _ncols = ncols;
+}
+
+
+
+//Defines the Destructor Operation
+template <class fp_type>
+SparseMatrix<fp_type>::~SparseMatrix()
+{
+    _nrows = -1;
+    _ncols = -1;
+    cout << "Called SpMV::SparseMatrix<fp_type> destructor!" << endl;
+}
+
